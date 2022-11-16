7bfa3d3b48deb0be23d5d9f468325be8ad271187
tom <tom@LAPTOP-RG75SD3O>
2022-11-16 01:26:45 -0300

    wena ql maraco

---
+++ src/controlador/registroVideo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import modelo.*;// reconoce modelo
+import java.sql.*; // TODO SQL
+import bd.Conexion; // comunicación BD
+import java.util.ArrayList; // SELECT 
+
+public class registroVideo {
+    
+}
