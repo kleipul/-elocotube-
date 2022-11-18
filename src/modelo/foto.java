ab03af7e0d5586da49e71c71bea54ed8598f5b0b
tom <tom@LAPTOP-RG75SD3O>
2022-11-17 22:49:29 -0300

    se completa clase foto

--- src/modelo/foto.java
+++ src/modelo/foto.java
@@ -7,12 +7,53 @@ public class foto {
     private String relacionAspecto;
     private String descripcion;
 
-    public foto(int nroFoto, String formato, String relacionAspecto, String descripcion) {
+    public foto() {
+        this.nroFoto = 0;
+        this.formato = "no info";
+        this.relacionAspecto = "no info";
+        this.descripcion = "no info";
+    }
+
+    public int getNroFoto() {
+        return nroFoto;
+    }
+
+    public void setNroFoto(int nroFoto) {
         this.nroFoto = nroFoto;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
         this.formato = formato;
+    }
+
+    public String getRelacionAspecto() {
+        return relacionAspecto;
+    }
+
+    public void setRelacionAspecto(String relacionAspecto) {
         this.relacionAspecto = relacionAspecto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
+    @Override
+    public String toString() {
+        return "foto{" + "nroFoto=" + nroFoto + ", formato=" + formato 
+                + ", relacionAspecto=" + relacionAspecto + ", descripcion=" 
+                + descripcion + '}';
+    }
+    
+    
     
     
 }
