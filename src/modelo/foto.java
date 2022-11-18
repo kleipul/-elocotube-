27a370a3836ce19ac676ea341d4a9f1021c1aca8
tom <tom@LAPTOP-RG75SD3O>
2022-11-17 22:44:21 -0300

    se crea clase foto

---
+++ src/modelo/foto.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class foto {
+
+    private int nroFoto;
+    private String formato;
+    private String relacionAspecto;
+    private String descripcion;
+
+    public foto(int nroFoto, String formato, String relacionAspecto, String descripcion) {
+        this.nroFoto = nroFoto;
+        this.formato = formato;
+        this.relacionAspecto = relacionAspecto;
+        this.descripcion = descripcion;
+    }
+    
+    
+}
